19de38a6f6a8967b7ad819968aab4487e1ebbddc
Akshay Bargaje <akshay.bargaje@deeptek.ai>
2024-03-17 11:29:51 +0530

    restored chnages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,12 @@ import { DoctorProfile } from "./api/api";
 import DocSchedule from "./components/Appointment/DocSchedule/DocSchedule";
 import DocData from "./components/Doctor/DocData";
 import Register from "./pages/Register";
-import Home from "./components/Home/Home";
-import Login from "./components/Register/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" />
       <Route path="/register" element={<Register />} />
-      <Route path="/register/user" element={<Login />} />
       <Route path="/user/:id" element={<Register />} />
       <Route path="/doctor/:id" element={<DocData />} />
     </Routes>
